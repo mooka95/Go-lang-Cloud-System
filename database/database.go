7606f8c148c8d761a687114ee8bd35bfd8e85fc1
mohamed Rabie <mohamed.osa.22@gmail.com>
2024-07-07 16:52:02 +0300

    update db

--- database/database.go
+++ database/database.go
@@ -57,59 +57,67 @@ func Init() {
 func createTables() error {
 	createTableStmt := `
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-        CREATE TABLE IF NOT EXISTS public.users (
-            id SERIAL PRIMARY KEY,
-            first_name VARCHAR(20) NOT NULL,
-            last_name VARCHAR(20) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            identifier VARCHAR(255) DEFAULT uuid_generate_v4() NOT NULL,
-            CONSTRAINT users_email_key UNIQUE (email),
-            CONSTRAINT users_identifier_key UNIQUE (identifier)
-        );
 
-        CREATE TABLE IF NOT EXISTS public.addresses (
-            id SERIAL PRIMARY KEY,
-            city VARCHAR(255) NOT NULL,
-            street VARCHAR(255) NOT NULL,
-            country VARCHAR(255) NOT NULL,
-            identifier VARCHAR(255) NOT NULL,
-            user_id INT NOT NULL,
-            CONSTRAINT unique_address_user_id UNIQUE (city, street, country, user_id),
-            CONSTRAINT address_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
-        );
+    DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'os_type') THEN
+            CREATE TYPE public.os_type AS ENUM ('Linux', 'Windows', 'macOS');
+        END IF;
+    END$$;
 
-        CREATE TABLE IF NOT EXISTS public.firewalls (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(20) NOT NULL,
-            identifier VARCHAR(255) NOT NULL,
-            user_id INT NULL,
-            CONSTRAINT firewalls_identifier_key UNIQUE (identifier),
-            CONSTRAINT unique_firewall_name_user_id UNIQUE (name, user_id),
-            CONSTRAINT firewalls_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
-        );
+    CREATE TABLE IF NOT EXISTS public.users (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(20) NOT NULL,
+        last_name VARCHAR(20) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        identifier VARCHAR(255) DEFAULT uuid_generate_v4() NOT NULL,
+        CONSTRAINT users_email_key UNIQUE (email),
+        CONSTRAINT users_identifier_key UNIQUE (identifier)
+    );
 
-        CREATE TABLE IF NOT EXISTS public.virtualmachines (
-            id SERIAL PRIMARY KEY,
-            hostname VARCHAR(255) NOT NULL,
-            operating_system public.os_type NULL,
-            is_active BOOLEAN NOT NULL,
-            identifier VARCHAR(255) DEFAULT uuid_generate_v4() NOT NULL,
-            user_id INT NOT NULL,
-            CONSTRAINT virtualmachines_identifier_key UNIQUE (identifier),
-            CONSTRAINT virtualmachines_unique UNIQUE (hostname),
-            CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id)
-        );
+    CREATE TABLE IF NOT EXISTS public.addresses (
+        id SERIAL PRIMARY KEY,
+        city VARCHAR(255) NOT NULL,
+        street VARCHAR(255) NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        identifier VARCHAR(255) NOT NULL,
+        user_id INT NOT NULL,
+        CONSTRAINT unique_address_user_id UNIQUE (city, street, country, user_id),
+        CONSTRAINT address_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
+    );
 
-        CREATE TABLE IF NOT EXISTS public.virtualmachines_firewalls (
-            id SERIAL PRIMARY KEY,
-            identifier VARCHAR(255) NOT NULL,
-            virtualmachine_id INT NOT NULL,
-            firewall_id INT NOT NULL,
-            CONSTRAINT unique_combination UNIQUE (virtualmachine_id, firewall_id),
-            CONSTRAINT firewall_id_fkey FOREIGN KEY (firewall_id) REFERENCES public.firewalls(id) ON DELETE CASCADE,
-            CONSTRAINT virtualmachine_id_fkey FOREIGN KEY (virtualmachine_id) REFERENCES public.virtualmachines(id) ON DELETE CASCADE
-        );
+    CREATE TABLE IF NOT EXISTS public.firewalls (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL,
+        identifier VARCHAR(255) NOT NULL,
+        user_id INT NULL,
+        CONSTRAINT firewalls_identifier_key UNIQUE (identifier),
+        CONSTRAINT unique_firewall_name_user_id UNIQUE (name, user_id),
+        CONSTRAINT firewalls_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.virtualmachines (
+        id SERIAL PRIMARY KEY,
+        hostname VARCHAR(255) NOT NULL,
+        operating_system public.os_type NULL,
+        is_active BOOLEAN NOT NULL,
+        identifier VARCHAR(255) DEFAULT uuid_generate_v4() NOT NULL,
+        user_id INT NOT NULL,
+        CONSTRAINT virtualmachines_identifier_key UNIQUE (identifier),
+        CONSTRAINT virtualmachines_unique UNIQUE (hostname),
+        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS public.virtualmachines_firewalls (
+        id SERIAL PRIMARY KEY,
+        identifier VARCHAR(255) NOT NULL,
+        virtualmachine_id INT NOT NULL,
+        firewall_id INT NOT NULL,
+        CONSTRAINT unique_combination UNIQUE (virtualmachine_id, firewall_id),
+        CONSTRAINT firewall_id_fkey FOREIGN KEY (firewall_id) REFERENCES public.firewalls(id) ON DELETE CASCADE,
+        CONSTRAINT virtualmachine_id_fkey FOREIGN KEY (virtualmachine_id) REFERENCES public.virtualmachines(id) ON DELETE CASCADE
+    );
     `
 
 	_, err := DB.Exec(createTableStmt)
